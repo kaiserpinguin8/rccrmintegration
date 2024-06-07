070167a1a575d751a36bed87881b6bbaf805e0d9
kaiserpinguin8 <132837926+kaiserpinguin8@users.noreply.github.com>
2024-06-07 12:40:28 +0200

    Add files via upload

---
+++ code/ringcentral.js
@@ -0,0 +1,33 @@
+// Add the RingCentral Web Widget
+(function() {
+  var rcs = document.createElement("script");
+  rcs.src = "https://ringcentral.github.io/ringcentral-web-widget/adapter.js?stylesUri=https://embbnux.github.io/ringcentral-web-widget-styles/GameofThrones/styles.css";
+  //https://ringcentral.github.io/ringcentral-web-widget/app.html?stylesUri=https://embbnux.github.io/ringcentral-web-widget-styles/GameofThrones/styles.css
+  var rcs0 = document.getElementsByTagName("script")[0];
+  rcs0.parentNode.insertBefore(rcs, rcs0);
+  if (window.RCAdapter) {
+    window.RCAdapter.setMinimized(false);
+  }
+})();
+
+// Inbound Screen Pop Based in Message Event
+(function () {
+  window.addEventListener('message', function(e) {
+    const data = e.data;
+    if (data) {
+      switch (data.type) {
+        case 'rc-call-ring-notify':
+          var id = number2id(data.call.from, number2user);
+          if (id) {
+            var contact = id2user[id];
+            window.title = contact.character.displayName;
+            window.history.pushState("", contact.character.displayName, "?id="+id);
+            loadSingleUser(id, id2user);
+          }
+          break;
+        default:
+          break;
+      }
+    }
+  })
+})();
